c921a192dd7bb6b5e402e29b454fb6eb06e0250f
Morgan Benton <morgan.benton@gmail.com>
2022-04-25 10:00:41 -0400

    added main function

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 """
 Primarily used to launch the program
 """
+
+
+def main():
+    """
+    This will setup the infrastructure for and
+    run the game.
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    main()
